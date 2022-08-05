71b65a28f56add135480a7e6f4f640f120cfc58a
Ana Clara Custodio <anaccustodio8@gmail.com>
2022-08-05 14:19:17 -0300

    criando mais um componente de conteudo da pagina

--- src/components/ContentContainer/index.js
+++ src/components/ContentContainer/index.js
@@ -1,4 +1,5 @@
 import { ContentRowMovies } from "../ContentRowMovies"
+import { LastMovieInDb } from "../LastMovieInDb"
 
 export const ContentContainer = () => {
     return(
@@ -6,7 +7,10 @@ export const ContentContainer = () => {
 			<div className="d-sm-flex align-items-center justify-content-between mb-4">
 				<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
 		    </div>
-            < ContentRowMovies />
+                < ContentRowMovies />
+            <div class="row">
+                <LastMovieInDb />
+            </div>
         </div>
     )
 }
